b1a13fafc900b03813e26ef8d082dae9dc71ea12
DarthLuisDa <5cvargas.luis@gmail.com>
2023-11-20 10:32:00 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+//Evento para crear un nuevo libro
+document.getElementById("formulario").addEventListener("submit", crear);
+
+//Funcion Crear
+
+function crear(e){
+    titulo = document.getElementById("titulo").value
+    descripcion = document.getElementById("descripcion").value
+    precio = document.getElementById("precio").value
+
+    let libro = {
+        titulo,
+        descripcion,
+        precio
+    }
+
+    if(localStorage.getItem("Libros") === null) {
+        let libros = []
+        libros.push(libro)
+        localStorage.setItem("Libros", JSON.stringify(libros)) 
+    }else{
+        let libros = JSON.parse(localStorage.getItem("Libros"))
+        libros.push(libro)
+        localStorage.setItem("Libros", JSON.stringify(libros))
+    }
+    leer();
+    document.getElementById("formulario").reset();
+    console.log("Libro guardado correctamente");
+    e.preventDefault()
+
+}
+
+
+//Funcion Leer
+
+function leer(){
+     let libros = JSON.parse(localStorage.getItem("Libros"));
+     document.getElementById("tbody").innerHTML = ""
+     for(let i=0; i < libros.length; i++){
+         let titulo = libros[i].titulo
+         let descripcion = libros[i].descripcion
+         let precio = libros[i].precio
+
+         document.getElementById("tbody").innerHTML +=
+         `<tr>
+              <td>${titulo}</td>
+              <td>${descripcion}</td>
+              <td>${precio}</td>
+         </tr>
+         `
+     }
+}
+
+leer();
